2402bd030f10b8b9fa2fc30516e89dcb0e64c4ff
johnalalade <75532394+johnalalade@users.noreply.github.com>
2021-10-21 12:15:38 +0100

    Update Landing.js

--- src/Screens/Landing.js
+++ src/Screens/Landing.js
@@ -1,7 +1,7 @@
 // import Video from './video.mp4';
 // import Image2 from "../img/handshake.svg"
 // import Image3 from '../img/seo_monochromatic.svg'
-import Image1 from '../img/E3D3C89B-AEBB-4C35-9BB4-676A12DAA932.jpeg'
+import Image1 from '1286AB83-7810-4AE1-A47B-E8B2AB8CA342.jpeg'
 // import MailIcon from '../img/mailicon.png'
 // import PhoneIcon from '../img/phoneicon.jfif'
 // import InternetIcon from '../img/interneticon.jfif'
